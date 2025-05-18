f327c1ad7004bd4da097532cd11771a5d294fae3
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-18 21:29:15 +0900

    [payment-practice]feat: add /create-checkout-session route to backend for Stripe payment session

--- payment-practice/backend/server.ts
+++ payment-practice/backend/server.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-const express = require("express");
+import stripe from "./stripe";
 
 const app = express();
 
@@ -8,6 +10,33 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("hello, express");
 });
 
+app.post("/create-checkout-session", async (req: Request, res: Response) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "T-shirt",
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      ui_mode: "custom",
+      return_url: "https://example.com/return?session_id={CHECKOUT_SESSION_ID}",
+    });
+
+    res.json({ checkoutSessionClientSecret: session.client_secret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+  console.log("Running on port 3000");
 });
