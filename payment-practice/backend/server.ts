71e4c25d4ce07b7da557214cfb1648119abbc59a
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-20 20:18:09 +0900

    [payment-practice]feat: integrate paypal order creation and capture APIs in backend

--- payment-practice/backend/server.ts
+++ payment-practice/backend/server.ts
@@ -1,6 +1,15 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import {
+  ApiError,
+  CheckoutPaymentIntent,
+  Client,
+  Environment,
+  LogLevel,
+  OrdersController,
+} from "@paypal/paypal-server-sdk";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -15,6 +24,139 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(bodyParser.json());
+
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8080 } = process.env;
+
+const client = new Client({
+  clientCredentialsAuthCredentials: {
+    oAuthClientId: PAYPAL_CLIENT_ID!,
+    oAuthClientSecret: PAYPAL_CLIENT_SECRET!,
+  },
+  timeout: 0,
+  environment: Environment.Sandbox,
+  logging: {
+    logLevel: LogLevel.Info,
+    logRequest: {
+      logBody: true,
+    },
+    logResponse: {
+      logHeaders: true,
+    },
+  },
+});
+
+const ordersController = new OrdersController(client);
+
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+const createOrder = async (cart: any) => {
+  const collect = {
+    body: {
+      intent: CheckoutPaymentIntent.Capture,
+      purchaseUnits: [
+        {
+          amount: {
+            currencyCode: "USD",
+            value: "100.00",
+          },
+        },
+      ],
+    },
+    prefer: "return=minimal",
+  };
+
+  try {
+    const { body, ...httpResponse } = await ordersController.createOrder(
+      collect
+    );
+    // Get more response info...
+    // const { statusCode, headers } = httpResponse;
+
+    if (typeof body !== "string") {
+      throw new Error("Unexpected body type: expected a string");
+    }
+    return {
+      jsonResponse: JSON.parse(body),
+      httpStatusCode: httpResponse.statusCode,
+    };
+  } catch (error) {
+    if (error instanceof ApiError) {
+      // const { statusCode, headers } = error;
+      throw new Error(error.message);
+    }
+  }
+};
+
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async (orderID: string) => {
+  const collect = {
+    id: orderID,
+    prefer: "return=minimal",
+  };
+
+  try {
+    const { body, ...httpResponse } = await ordersController.captureOrder(
+      collect
+    );
+    // Get more response info...
+    // const { statusCode, headers } = httpResponse;
+
+    if (typeof body !== "string") {
+      throw new Error("Unexpected body type: expected a string");
+    }
+
+    return {
+      jsonResponse: JSON.parse(body),
+      httpStatusCode: httpResponse.statusCode,
+    };
+  } catch (error) {
+    if (error instanceof ApiError) {
+      // const { statusCode, headers } = error;
+      throw new Error(error.message);
+    }
+  }
+};
+
+app.post("/api/orders", async (req, res) => {
+  try {
+    // use the cart information passed from the front-end to calculate the order amount detals
+    const { cart } = req.body;
+    const result = await createOrder(cart);
+
+    if (!result) {
+      throw new Error("createOrder returned undefined");
+    }
+    const { jsonResponse, httpStatusCode } = result;
+
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to create order." });
+  }
+});
+
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  try {
+    const { orderID } = req.params;
+    const result = await captureOrder(orderID);
+
+    if (!result) {
+      throw new Error("captureOrder returned undefined");
+    }
+    const { jsonResponse, httpStatusCode } = result;
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to capture order." });
+  }
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("hello, express");
@@ -47,6 +189,10 @@ app.post("/create-checkout-session", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Running on port 3000");
+// app.listen(3000, () => {
+//   console.log("Running on port 3000");
+// });
+
+app.listen(PORT, () => {
+  console.log(`Node server listening at http://localhost:${PORT}/`);
 });
